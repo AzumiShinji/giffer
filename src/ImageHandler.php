dc645c6e8180c01859c5ea1a680052b431b520cb
Artem <Azumishinji.jp@gmail.com>
2022-05-03 05:53:35 +0300

    Initial commit

---
+++ src/ImageHandler.php
@@ -0,0 +1,309 @@
+<?php 
+
+/**
+ * Устанавливаем зависимости библиотек
+ */
+require __DIR__ . '/../vendor/autoload.php';
+
+use GIFEndec\Events\FrameRenderedEvent;
+use GIFEndec\IO\FileStream;
+use GIFEndec\Decoder;
+use GIFEndec\Color;
+use GIFEndec\Encoder;
+use GIFEndec\Frame;
+use GIFEndec\Renderer;
+
+/**
+ * Обработка gif файлов
+ */
+class ImageHandler
+{
+    /**
+     * Путь до изображения
+     * @var string
+     */
+    protected $imgPath;
+
+    /**
+     * Расположение временного хранилища
+     * @var string
+     */
+    protected $tmpDir;
+
+    /**
+     * Ширина изображения
+     * @var int
+     */
+    protected $imgX;
+
+    /**
+     * Высота изображения
+     * @var int
+     */
+    protected $imgY;
+
+    /**
+     * Длина кадра в миллисекундах
+     * @var int[]
+     */
+    protected $framesDuration = [];
+
+    /**
+     * Количество кадров
+     * @var int
+     */
+    protected $numOfFrames;
+
+
+    /**
+     * @param string $path Путь до исходного изображения
+     * @param string $tmpDir Путь до временного хранилища кадров, по умолчанию /../images/frames/
+     */
+    public function __construct($path, $tmpDir = __DIR__ . "/../images/frames/")
+    {
+        $this->imgPath = $path;
+        $this->tmpDir = $tmpDir;
+        
+    }
+
+    /**
+     * Деструктор экземпляра класса, удаляет временную дирректорию для кадров
+     */
+    public function __destruct()
+    {
+        // Очищаем дирректорию от файлов
+        array_map('unlink', glob($this->tmpDir . "*"));
+        if(!rmdir($this->tmpDir)) throw new Exception("Ошибка. Код -1. Ошибка удаления временной директории.");
+    }
+
+    /**
+     * Разбор GIF анимации на кадры
+     */
+    public function DecodeGIF()
+    {
+        $gifStream = new FileStream($this->imgPath);
+        $gifDecoder = new Decoder($gifStream);
+        $gifRenderer = new Renderer($gifDecoder);
+
+        if (!mkdir($this->tmpDir)) throw new Exception("Ошибка. Код -1. Ошибка создания временного хранилища кадров.");
+
+        $gifRenderer->start(function (FrameRenderedEvent $event) {
+
+            // Задаем формат нумерации 000
+            $paddedIndex = str_pad($event->frameIndex, 3, '0', STR_PAD_LEFT);
+            // Сохраняем кадр в файл
+            imagegif($event->renderedFrame, $this->tmpDir ."frame{$paddedIndex}.gif");
+            // Сохраняем длину кадра
+            $this->framesDuration[] = $event->decodedFrame->getDuration();
+        });
+        // Получаем количество кадров
+        $this->numOfFrames = count($this->framesDuration);
+    }
+
+    /**
+     * Сборка GIF анимации в единый файл
+     * @param string $destinationPath 
+     */
+    public function EncodeGIF($destinationPath)
+    {
+        $gif = new Encoder();
+
+        $i = 0;
+        foreach (glob($this->tmpDir . 'frame*.gif') as $file) {
+            $stream = new FileStream($file);
+            $frame = new Frame();
+            $frame->setDisposalMethod(1);
+            $frame->setStream($stream);
+            $frame->setDuration($this->framesDuration[$i]);
+            $gif->addFrame($frame);
+            $i++;
+        }
+    
+        $gif->addFooter();
+    
+        // Сохраняем готовую анимацию в файл
+        $gif->getStream()->copyContentsToFile($destinationPath);
+
+    }
+    
+    /**
+     * 
+     * Изменение размера изображения
+     * 
+     * @param int $width Необходимая ширина изображения, если не указана устанавливается пропорционально новой высоте
+     * @param int $height Необходимая высота изображения, если не указана устанавливается пропорционально новой ширине
+     * @param bool $isWatermark Проверка на водяной знак
+     * @param GDImage $watermark Путь до изображения водяного знака
+     */
+    public function ImageResize($width = 0, $height = 0, $isWatermark = false, $watermark = 0)
+    {
+
+        $resizer = function($i=0, $wm = 0, $img = 0) use($width, $height)
+        {
+            // Загружаем кадр
+            if (!$wm)
+                $img = imagecreatefromgif($this->tmpDir . "frame" . str_pad($i, 3 , '0', STR_PAD_LEFT).'.gif');
+
+            $oldWidth = imagesx($img);
+            $oldHeight = imagesy($img);
+            
+            // Проверяем что пользователь ввел корректные значения
+            if (($width == 0 and $height == 0) or $width < 0 or $height < 0)
+            {
+                throw new Exception("Ошибка. Код -1. Вы ввели некорректные значения ширины и высоты. Изменения не будут внесены.");
+                
+            } elseif ($width == 0)
+            {
+                $newHeight = $height;
+                // Вычисляем ширину пропорциональную новой высоте
+                $newWidth = (int)($oldWidth / ( $oldHeight / $newHeight ));
+
+            } elseif ($height == 0)
+            {
+                $newWidth = $width;
+                // Вычисляем высоту пропорциональную новой ширине
+                $newHeight = (int)($oldHeight / ( $oldWidth / $newWidth ));
+                
+            } else
+            {
+                $newWidth = $width;
+                $newHeight = $height;
+            }
+        
+            // Создаем холст под изображение с измененными шириной и высотой
+            $newImg = imagecreatetruecolor($newWidth, $newHeight);
+            // Копируем оригинальное изображение, с изменением ширины и высоты, на полученный холст
+            imagecopyresized($newImg, $img, 0, 0, 0, 0, $newWidth, $newHeight, $oldWidth, $oldHeight);
+
+            // Сохраняем в файл
+            if (!$wm)
+                imagegif($newImg, $this->tmpDir . "frame" . str_pad($i, 3 , '0', STR_PAD_LEFT).'.gif');
+            else
+                imagepng($newImg, $this->tmpDir . "wm.png");
+            
+            // Удаляем кадры из памяти
+            imagedestroy($img);
+            imagedestroy($newImg);
+        };
+
+        // Проверяем, необходимо ли изменить только водяной знак        
+        if ($isWatermark)
+        {
+            $resizer(0, $isWatermark, $watermark);
+        }
+        else
+        {
+            // Циклично обрабатываем все кадры
+            $this->Loop($resizer, " Изменяем размер кадра ");
+        }
+        
+    }
+
+    /**
+     * Наложение водяного знака на изображение
+     * 
+     * @param string $watermarkPath Путь до изображения с водяным знаком
+     */
+    public function PlaceWatermark($watermarkPath)
+    {
+        // Загружаем изображение водяного знака в соответствии с форматом
+        switch(getimagesize($watermarkPath)["mime"])
+        {
+            case "image/png":
+                $stamp = imagecreatefrompng($watermarkPath);
+                break;
+            case "image/gif":
+                $stamp = imagecreatefromgif($watermarkPath);
+                break;
+            case "image/jpeg":
+                $stamp = imagecreatefromjpeg($watermarkPath);
+                break;
+                break;
+            case "image/bmp":
+                $stamp = imagecreatefrombmp($watermarkPath);
+                break;
+            default:
+                throw new Exception("Неверный формат файла");
+        }
+
+        // Получаем размеры кадров, для корректировки водяного знака
+        list($this->imgX, $this->imgY) = getimagesize($this->tmpDir . 'frame000.gif');
+
+        // Устанавливаем размер отступа
+        $marge_right = 10;
+        $marge_bottom = 10;
+        
+        // Получаем размер изображения водяного знака
+        $stampX = imagesx($stamp);
+        $stampY = imagesy($stamp);
+
+        // Если размер водяного знака по ширине или длине больше чем на 40% от ширины и длины исходного изображения,
+        // то уменьшаем его
+        if ($stampX > $this->imgX*0.40 or $stampY > $this->imgY*0.40)
+        {
+            if ($stampX > $this->imgX*0.40)
+            { 
+                $this->ImageResize((int)($this->imgX*0.40), isWatermark:true, watermark:$stamp);
+            }
+            $stamp = imagecreatefrompng($this->tmpDir . "wm.png");
+            $stampX = imagesx($stamp);
+            $stampY = imagesy($stamp); 
+
+            if ($stampY > $this->imgY*0.40)
+            {
+                $this->ImageResize(height:(int)($this->imgY*0.40), isWatermark:true, watermark:$stamp);
+            }
+            $stamp = imagecreatefrompng($this->tmpDir . "wm.png");
+            $stampX = imagesx($stamp);
+            $stampY = imagesy($stamp); 
+            
+        }
+
+        // Анонимная функция для наложения водяного знака на кадр
+        $watermarker = function($i) use($stamp, $stampX, $stampY, $marge_right, $marge_bottom) 
+        {
+            // Загружаем кадр
+            $frame = imagecreatefromgif($this->tmpDir . "frame" . str_pad($i, 3 , '0', STR_PAD_LEFT).'.gif');
+
+            // Получаем размер кадра
+            $imgX = imagesx($frame);
+            $imgY = imagesy($frame);
+
+            // Совмещаем кадр и водяной знак
+            imagecopymerge($frame, $stamp, $imgX - $stampX - $marge_right, $imgY - $stampY - $marge_bottom,0, 0, $stampX, $stampY,60);
+            // Сохраняем в файл
+            imagegif($frame, $this->tmpDir . "frame" . str_pad($i, 3 , '0', STR_PAD_LEFT).'.gif');
+            // Выгружаем кадр из памяти
+            imagedestroy($frame);
+        };
+
+        // Циклично обрабатываем все кадры
+        $this->Loop($watermarker, " Накладываем водяной знак ");
+            
+    }
+
+    /**
+     * Метод для цикличной обработки анонимных функций
+     * 
+     * @param function $task Анонимная функция
+     * @param string $info Подпись прогрессбара
+     */
+    private function Loop($task, $info)
+    {
+        for ($i = 0; $i < $this->numOfFrames; $i++)
+        {
+            $task($i);
+            /**
+             * Progressbar 
+             * @author: https://gist.github.com/mayconbordin/2860547
+             */
+            $perc = round(($i * 100) / $this->numOfFrames);
+            $bar = round((50 * $perc) / 100);
+            echo sprintf("%s%%[%s>%s]%s\r", $perc, str_repeat("=", $bar), str_repeat(" ", 50-$bar), $info);
+        }
+        echo "\n";
+    }
+
+}
+
+?>
---
+++ src/giffer.php
@@ -0,0 +1,99 @@
+#!C:\php\php.exe -q
+<?php
+
+require __DIR__ . '/ImageHandler.php';
+
+$width = 0;
+$height = 0;
+
+try
+{
+    $args = arguments($argv);
+
+    if ( $args['help'] )
+    {
+        echo "[СПРАВКА]
+        giffer [OPTIONS] SRC_FILE DEST_FILE
+        где
+        OPTIONS - входные параметры:
+            -w, --width - ширина изображения в пикселях (целочисленное число)
+            -h, --height - высота изображения в пикселях (целочисленное число)
+            -m, --mark - путь к файлу-картинки, которая используется в качестве водяного знака (строка)
+        
+        SRC_FILE - путь к исходному файлу для обработки (строка)
+        DEST_FILE - путь к выходному файлу (строка)
+        
+        Пример:
+        giffer --width=640 --height=480 --mark=\"C:\\mark.png\" \"C:\\orig.gif\" \"C:\\result.gif\" 
+        ";
+    }
+    elseif ( !$args['options'] or count( $args['options'] ) > 3 or count( $args['arguments'] ) != 2 )
+    {
+        throw new Exception("Ошибка: некорректная команда.\nДля вызова справки используйте '-?'");
+    }
+    elseif ( isset( $args['options']['w'], $args['options']['width'] ) or isset( $args['options']['h'], $args['options']['height'] ) or isset( $args['options']['m'], $args['options']['mark'] ) )
+            throw new Exception("Ошибка: введены две одинаковых опции.");
+    else
+    {
+        if ( isset( $args['options']['w']) )
+            $width = $args['options']['w'];
+        if ( isset( $args['options']['width']) )
+            $width = $args['options']['width'];
+            
+        if ( isset( $args['options']['h']) )
+            $height = $args['options']['h'];
+        if ( isset( $args['options']['height']) )
+            $height = $args['options']['height'];
+            
+        if ( isset( $args['options']['m']) )
+            $mark = $args['options']['m'];
+        if ( isset( $args['options']['mark']) )
+            $mark = $args['options']['mark'];
+
+        $src = $args['arguments'][0];
+        $dest = $args['arguments'][1];
+        
+    }    
+    
+    
+    $originalImg = new ImageHandler($src);
+    $originalImg->DecodeGIF();
+    $originalImg->ImageResize($width, $height);
+    if( $mark ) $originalImg->PlaceWatermark($mark);
+    $originalImg->EncodeGIF($dest);
+
+    echo "Программа завершена с кодом 0";
+}
+catch(Throwable $ex)
+{
+    echo "Программа завершена с ошибками -1\n{$ex->getMessage()}";
+}
+
+/**
+ * Получение опций и аргументов командной строки
+ * 
+ * @param array $_argv Массив аргументов командной строки
+ */
+function arguments($_argv) {
+    $options = [];
+    $args = array 
+    (
+        'options' => array(),
+        'arguments' => array(),
+        'help' => false
+    );
+
+    foreach($_argv as $arg)
+    {
+        if ( preg_match( '/-{1,2}([^=]+)=(.*)/', $arg, $matches ) ) 
+            $args['options'][$matches[1]] = $matches[2];
+        elseif ( substr($arg, -3) == "gif" )
+            $args['arguments'][] = $arg;
+        elseif ($arg === "-?")
+            $args['help'] = true;
+
+    }
+    return $args;
+}
+
+?>
